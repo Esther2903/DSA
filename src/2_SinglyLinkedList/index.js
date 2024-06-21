d1fc3d167b12a7ccc8acd4d05c83039f03598f9f
Raïssa Raïssath <raissaraissa376@gmail.com>
2024-06-21 16:38:21 +0100

    Exercise Treatment 2_Array

--- src/2_SinglyLinkedList/index.js
+++ src/2_SinglyLinkedList/index.js
@@ -7,57 +7,138 @@ class Node {
 }
 export class SinglyLinkedList {
   constructor(){
-    this.head = null
+    this.head = null;
   }
   prepend(value) {
     const newNode = new Node(value)
     if (!this.head) {
-        this.head = newNode
+        this.head = newNode;
     }else{
-      newNode.next = this.head
-      this.head = newNode
+      newNode.next = this.head;
+      this.head = newNode;
       
     }
   }
 
   append(value) {
-    let newNode = new Node(value)
+    let newNode = new Node(value);
     if (!this.head) {
         this.head = newNode
-        newNode.next = null
     }else{
-      let currentNode = this.head 
-      while (currentNode) {
-        currentNode = currentNode.next
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
       }
-      currentNode.next = newNode
+      currentNode.next = newNode;
     }
   }
 
   insertAt(index, value) {
     let newNode = new Node(value)
-    if (!this.head) {
-      this.head = newNode
-      newNode.next = null
+    if (index === 0) {
+      this.head = newNode;
+      newNode.next = this.head;
     }
     else{
-
+      let currentIndex = 0;
+      let previousNode = null;
+      let currentNode = this.head;
+      while (currentIndex < index) {
+        previousNode = currentNode;
+        currentNode = currentNode.next
+        currentIndex++;
+      }
+      previousNode.next = newNode;
+      newNode.next = currentNode;
     }
   }
 
-  removeLast() {}
+  removeLast() {
+    if (!this.head) {
+      return null;
+    }
+    else if(!this.head.next){
+      this.head = null
+    }
+    let currentNode = this.head;
+    let previousNode = null;
+    while (currentNode.next) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
+    if (previousNode) {
+      previousNode.next = null;
+    }
+  }
+  
 
-  removeFirst() {}
-
-  removeAt(index, value) {}
-
-  find(value) {}
-
-  getFirst() {
-    
+  removeFirst() {
+    if (!this.head) {
+        return null;
+    }else{
+      this.head = this.head.next;      
+    }
   }
 
-  getLast() {}
+  removeAt(index, value) {
+    if (index === 0) {
+      this.head = this.head.next;
+    }
+    else{
+      let currentIndex = 0;
+      let previousNode = null;
+      let currentNode = this.head;
+      while (currentIndex < index) {
+        previousNode = currentNode;
+        currentNode = currentNode.next
+        currentIndex++;
+      }
+      previousNode.next = currentNode.next;
+    }
+  }
 
-  getAt(index) {}
+  find(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if(currentNode.value === value){
+        return currentNode.value;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  
+  }
+
+  getFirst() {
+    if (!this.head) {
+      return null;
+    } 
+    return this.head.value;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    } 
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    return currentNode.value;
+  }
+
+  getAt(index) {
+    if(index < 0) {
+      return null;
+    }
+    let currentIndex = 0;
+    let currentNode = this.head;
+    while (currentNode) {
+      if(currentIndex === index){
+        return currentNode.value;
+      }
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+  }
 }
