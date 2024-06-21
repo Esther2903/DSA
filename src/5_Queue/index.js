4b3b232800caacf0d52d8fd5dff416109d0a6556
Raïssa Raïssath <raissaraissa376@gmail.com>
2024-06-21 20:08:23 +0100

    Exercise Treatment 5_Queue

--- src/5_Queue/index.js
+++ src/5_Queue/index.js
@@ -1,8 +1,42 @@
 // Do not use arrays!
+class Node {
+  constructor(value) {
+    this.value=value;
+    this.next=null;
+  }
+}
 export class Queue {
-  enqueue(value) {}
+  constructor() {
+    this.first=null;
+    this.last=null
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if(!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+    else{
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+  }
 
-  dequeue() {}
+  dequeue() {
+    if(!this.first) {
+      return null;
+    }
+    else {
+      const value = this.first.value;
+      this.first = this.first.next;
+      if (!this.first) {
+        this.last = null;
+      }
+      return value;
+    }
+  }
 
-  peek() {}
+  peek() {
+    return this.first.value;
+  }
 }
