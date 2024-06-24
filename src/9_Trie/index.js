171af5068a13479708507c76b43bd4ac96e75923
Raïssa Raïssath <raissaraissa376@gmail.com>
2024-06-24 12:33:40 +0100

    Exercise Treatment 9_Tie

--- src/9_Trie/index.js
+++ src/9_Trie/index.js
@@ -1,5 +1,43 @@
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
 export class Trie {
-  insert(word) {}
+  constructor() {
+    this.root = new TrieNode();
+  }
+  insert(word) {
+    let node = this.root;
+    for (const char of word) {
+      if(!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];  
+    }
+    node.isEndOfWord = true;
+  }
 
-  retrieve(word) {}
+  retrieve(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        return [];
+      }
+      node = node.children[char];
+    }
+    return this.retrieveWords(node, word)
+  }
+
+  retrieveWords(node, word) {
+    let words = []; 
+    if (node.isEndOfWord) {
+      words.push(word);
+    }
+    for (let char in node.children) {
+      words = words.concat(this.retrieveWords(node.children[char], word + char));
+    }
+    return words;
+  }
 }
