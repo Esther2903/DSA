71c735221cd756590c598023fcc3b67c075641ba
Raïssa Raïssath <raissaraissa376@gmail.com>
2024-06-24 10:41:24 +0100

    Exercise Treatment 6_HashTable

--- src/6_HashTable/index.js
+++ src/6_HashTable/index.js
@@ -1,13 +1,59 @@
 export class HashTable {
-  constructor(bucketsNum = 10) {}
+  constructor(bucketsNum = 10) {
+    this.bucketsNum = bucketsNum;
+    this.buckets = Array.from({length: bucketsNum}, () => []);
+    this.numItems = 0;
+  }
 
-  get size() {}
+  get size() {
+    return this.numItems;
+  }
 
-  #hash(key) {}
+  #hash(key) {
+    let hash = 0;
+    for (let i = 0; i< key.length; i++) {
+      hash += key.charCodeAt(i);    
+    }
+    return hash % this.bucketsNum;
+  }
 
-  set(key, value) {}
+  set(key, value) {
+    const index = this.#hash(key);
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, storedValue] = bucket[i];
+      if(storedKey === key) {
+        bucket[i] = [key, value];
+        return;
+      } 
+    }
+    bucket.push([key,value]);
+    this.numItems++;
+  }
 
-  get(key) {}
+  get(key) {
+    const index = this.#hash(key);
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, storedValue] = bucket[i];
+      if(storedKey === key) {
+        return storedValue;
+      } 
+    }
+    return null;
+  }
 
-  remove(key) {}
+  remove(key) {
+    const index = this.#hash(key);
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, storedValue] = bucket[i];
+      if(storedKey === key) {
+        bucket.splice(i,1);
+        this.numItems--;
+        return true;
+      } 
+    }
+    return false; 
+  } 
 }
