311d098743140829f4ee635858ba572abd3b5970
Raïssa Raïssath <raissaraissa376@gmail.com>
2024-06-27 06:23:16 +0100

    Exercise Treatment 7_Tree

--- src/7_Tree/index.js
+++ src/7_Tree/index.js
@@ -1,13 +1,61 @@
+class Node {
+
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    this.children.push(newNode);
+    return newNode;
+  }
+}
+
 export class Tree {
+  constructor() {
+    this.root = null;
+  }
   // Return the newly inserted node/sub-tree
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    }
+    return newNode;
+  }
 
   // Breadth-first search
-  BFS(visitor) {}
+  BFS(visitor) {
+    if (!this.root) return;
+
+    const queue = [this.root];
+    while (queue.length > 0) {
+      const node = queue.shift();
+      visitor(node.value);
+      queue.push(...node.children);
+    }
+  }
 
   // Pre-order depth-first search
-  preOrderDFS(visitor) {}
+  preOrderDFS(visitor) {
+    this.preOrder(this.root, visitor);
+  }
+
+  preOrder(node, visitor) {
+    if (!node) return;
+    visitor(node.value);
+    node.children.forEach(child => this.preOrder(child, visitor));
+  }
 
   // Post-order depth-first search
-  postOrderDFS(visitor) {}
+  postOrderDFS(visitor) {
+    this.postOrder(this.root, visitor);
+  }
+
+  postOrder(node, visitor) {
+    if (!node) return;
+    node.children.forEach(child => this.postOrder(child, visitor));
+    visitor(node.value);
+  }
 }
